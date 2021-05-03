e70c68d66453c41823d32421c448394fca0530e3
Count Zero <joelecox1@gmail.com>
2021-05-03 12:44:53 -0500

    checkDatabase function and event listener

--- public/js/idb.js
+++ public/js/idb.js
@@ -27,3 +27,38 @@ function saveRecord(record) {
   const store = transaction.objectStore('pending')
   store.add(record)
 }
+
+function checkDatabase() {
+  const transaction = db.transaction(['pending'], 'readwrite')
+  const store = transaction.objectStore('pending')
+  const getAll = store.getAll()
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(['pending'], 'readwrite')
+          const store = transaction.objectStore('pending')
+
+          store.clear();
+          alert('All saved transactions have been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+}
+
+window.addEventListener('online', checkDatabase) 
